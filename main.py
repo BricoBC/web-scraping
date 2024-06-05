0a8031bdee5e56e6287a48cbcc2a04cfa81fad80
BricoBC <barajas.c.bruno.n@gmail.com>
2024-06-04 19:23:13 -0600

    Add sentence try-finally and time.sleep for 5 seconds

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import Select
 
+try:   
+    service = Service('D:\\Programas\\edgedriver_win64\\msedgedriver.exe')
+    options = Options()
 
-service = Service('D:\\Programas\\edgedriver_win64\\msedgedriver.exe')
-options = Options()
+    driver = webdriver.Edge(service=service, options=options)
+    website = 'https://www.adamchoi.co.uk/'
 
-driver = webdriver.Edge(service=service, options=options)
-website = 'https://www.adamchoi.co.uk/'
+    driver.get(website)
 
-driver.get(website)
+    btn_win = driver.find_element(By.XPATH, '//*[@id="side-menu"]/li[13]/a')
+    btn_win.click()
 
+    
+finally:
+    time.sleep(5) 
+    driver.quit()
